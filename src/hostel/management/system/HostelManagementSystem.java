56c58e81dbcb55f2438c40ec20c846a8dd4fbe94
Pawan Lulla <pawanlulla021@gmail.com>
2021-06-19 22:12:01 +0530

    initial commit

---
+++ src/hostel/management/system/HostelManagementSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hostel.management.system;
+
+/**
+ *
+ * @author Dell
+ */
+public class HostelManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
